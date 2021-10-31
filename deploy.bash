b2ff330a86df904033a4a27ed06f8bbc9cb309fa
Shuai <shuai@ShuaideMacBook-Pro.local>
2021-10-31 10:03:44 +0800

    new post

--- deploy.bash
+++ deploy.bash
@@ -1,5 +1,7 @@
+cd ..
 hugo --theme="gokarna" --baseUrl="https://ncepuzs.github.io" --buildDrafts
 echo "================1. Already complete deployment========================="
+cd public
 git add .
 git commit -m "new post"
 export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
