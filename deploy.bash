c4458263c468703df9b96e54a5b6657260c08f4a
Shuai <shuai@ShuaideMacBook-Pro.local>
2021-10-31 10:02:38 +0800

    new post

--- deploy.bash
+++ deploy.bash
@@ -1,13 +1,14 @@
+hugo --theme="gokarna" --baseUrl="https://ncepuzs.github.io" --buildDrafts
+echo "================1. Already complete deployment========================="
 git add .
 git commit -m "new post"
 export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
-echo "1. Already open proxy with 127.0.0.1:7890"
+echo "================2. Already open proxy with 127.0.0.1:7890=============="
 
 git push origin master
-hugo --theme="gokarna" --baseUrl="https://ncepuzs.github.io" --buildDrafts
-echo "2. Already complete deploy on GitHub"
+echo "================3. Already push to GitHub=============================="
 
 unset http_proxy
 unset https_proxy
 
-echo "Already close proxy"
+echo "================4. Already close proxy================================="
